8ed4aa79ff601cb73058e60910d26dc04791aa99
David Pett <dpett@folklore.digital>
2016-03-23 09:31:21 -0500

    blueprints

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
-  name: 'ember-data-contentful'
+  name: 'ember-data-contentful',
+
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  }
 };
